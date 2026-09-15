e7236877f61a1786429a51447f9232d5d1add83b
Prince <prince@kodlabs.in>
2026-09-15 19:38:05 +0530

    Initial Commit

---
+++ Whyboard/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Whyboard
+//
+//  Created by Prince on 15/09/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Whyboard/WhyboardApp.swift
@@ -0,0 +1,32 @@
+//
+//  WhyboardApp.swift
+//  Whyboard
+//
+//  Created by Prince on 15/09/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WhyboardApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
